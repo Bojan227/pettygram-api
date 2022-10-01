127f01c0f1e23c526d6e1b0a3f819d4d2a760426
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-10-01 13:16:50 +0200

    Add require auth middleware function for authorization

---
+++ src/middleware/requireAuth.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+export const requireAuth = async (req: any, res: any, next: any) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization token required' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  try {
+    const _id = jwt.verify(token, process.env.SECRET);
+    req.user = await User.find({ _id }).select('_id');
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Request is not authorized' });
+  }
+};
