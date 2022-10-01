d44d0c203257cec60cc20da3686601181862fc9b
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-10-01 13:09:58 +0200

    Add post routes

---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+const router = express.Router();
+import {
+  createPost,
+  getPosts,
+  deletePost,
+  getPostById,
+  updateLikes,
+  getSpecificPostComments,
+} from '../controllers/postController';
+import { requireAuth } from '../middleware/requireAuth';
+
+router.get('/', getPosts);
+
+router.get('/:postId', getPostById);
+router.get('/:postId/comments', getSpecificPostComments);
+
+router.post('/', requireAuth, createPost);
+
+router.put('/', updateLikes);
+
+router.delete('/', deletePost);
+export default router;
