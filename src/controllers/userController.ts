5d34536a20d315f9a7e809ef923e65e991c71d90
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-12 13:39:39 +0100

    Save users as objects in the followers and following fields

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -85,21 +85,36 @@ export const login = async (req: any, res: any) => {
 export const updateUserData = async (req: any, res: any) => {
   const { userId } = req.body;
 
-  const { following } = await User.findOne({ _id: req.user[0] });
-  const { followers } = await User.findOne({
-    _id: new mongoose.Types.ObjectId(userId),
-  });
+  const loggedUser = await User.findOne({ _id: req.user[0] }, { password: 0 });
+  const { username, firstName, lastName, imageUrl, followers, following } =
+    await User.findOne({
+      _id: new mongoose.Types.ObjectId(userId),
+    });
+
+  const newUser = {
+    _id: userId,
+    username,
+    firstName,
+    lastName,
+    imageUrl,
+    followers,
+    following,
+  };
+
   const user = await User.findOneAndUpdate(
     { _id: req.user[0] },
+
     {
       $set: {
-        following: following.includes(userId)
-          ? following.filter((id) => id !== userId)
-          : [...following, userId],
+        following: loggedUser.following.find(({ _id }) => _id === userId)
+          ? loggedUser.following.filter(({ _id }) => _id !== userId)
+          : [...loggedUser.following, newUser],
       },
     },
+
     {
       returnOriginal: false,
+      password: 0,
     }
   );
 
@@ -107,16 +122,18 @@ export const updateUserData = async (req: any, res: any) => {
     { _id: new mongoose.Types.ObjectId(userId) },
     {
       $set: {
-        followers: followers.includes(req.user[0]._id)
-          ? followers.filter((id) => id !== req.user[0]._id.toString())
-          : [...followers, req.user[0]._id],
+        followers: followers.find(
+          ({ _id }) => _id === req.user[0]._id.toString()
+        )
+          ? followers.filter(({ _id }) => _id !== req.user[0]._id.toString())
+          : [...followers, user],
       },
     },
     { returnOriginal: false }
   );
 
   if (user) {
-    return res.status(200).json({ user });
+    return res.status(200).json({ user, newUser });
   } else {
     return res.status(404).json({ error: 'User unknown' });
   }
