d6e1e554847c4864e90cf6a8eeb667dc522bab90
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-12 13:20:19 +0100

    Update user fields to be required

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: String,
-  password: String,
-  firstName: String,
-  lastName: String,
-  imageId: String,
-  imageUrl: String,
-  followers: [String],
-  following: [String],
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  imageId: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  followers: { type: [String], required: true },
+  following: { type: [String], required: true },
 });
 
 const myDB = mongoose.connection.useDb('insta');
