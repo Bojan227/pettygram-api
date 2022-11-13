06b23427025465a93313bcfc1d5cfbc80c5c81db
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-13 15:16:09 +0100

    Update saved routes

--- src/routes/savedRoute.ts
+++ src/routes/savedRoute.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { requireAuth } from '../middleware/requireAuth';
 const router = express.Router();
-import { getSavedItemsByUserId } from '../controllers/savedController';
+import {
+  getSavedItemsByUserId,
+  updateSavedPosts,
+} from '../controllers/savedController';
 
-router.get('/:userId', getSavedItemsByUserId);
+router.use(requireAuth);
+
+router.get('/', getSavedItemsByUserId);
+router.put('/', updateSavedPosts);
 
 export default router;
