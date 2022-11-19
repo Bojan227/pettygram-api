ae46a382b50d9a414a41f6690f1ab25ade19471e
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-19 17:51:32 +0100

    Add update saved posts based on post id sent from client

--- src/controllers/savedController.ts
+++ src/controllers/savedController.ts
@@ -1,5 +1,6 @@
 import User from '../models/userModel';
 import Post from '../models/postModel';
+import mongoose from 'mongoose';
 
 export const getSavedItemsByUserId = async (req: any, res: any) => {
   try {
@@ -7,5 +8,29 @@ export const getSavedItemsByUserId = async (req: any, res: any) => {
     res.status(200).json(saved);
   } catch (error) {
     res.status(400).json({ msg: 'No saved posts' });
+  }
+};
+
+export const updateSavedPosts = async (req: any, res: any) => {
+  const { postId } = req.body;
+
+  const { saved } = await User.findOne({ _id: req.user[0] });
+  const post = await Post.findOne({ _id: new mongoose.Types.ObjectId(postId) });
+
+  const update = {
+    $set: {
+      saved: saved.find(({ _id }) => _id.toString() === postId)
+        ? saved.filter(({ _id }) => _id.toString() !== postId)
+        : [...saved, post],
+    },
+  };
+
+  try {
+    const user = await User.findOneAndUpdate({ _id: req.user[0] }, update, {
+      returnOriginal: false,
+    }).select('-password');
+    res.status(200).json({ user, post });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
