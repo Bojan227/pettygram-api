0ebca257a82cea47080c5b17a4638a5c36ff4efe
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-13 16:49:55 +0100

    update chat schema

--- src/models/chatModel.ts
+++ src/models/chatModel.ts
@@ -2,13 +2,15 @@ import mongoose from 'mongoose';
 import User from './userModel';
 const { Schema } = mongoose;
 
-const ChatSchema = new Schema({
-  room: String,
-  author: { type: Schema.Types.ObjectId, ref: User },
-  receiver: { type: Schema.Types.ObjectId, ref: User },
-  message: String,
-  time: String,
-});
+const ChatSchema = new Schema(
+  {
+    members: Array,
+    senderId: { type: Schema.Types.ObjectId, ref: User },
+    receiverId: { type: Schema.Types.ObjectId, ref: User },
+    message: String,
+  },
+  { timestamps: true }
+);
 
 const myDB = mongoose.connection.useDb('insta');
 
