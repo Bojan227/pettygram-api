b14b3063f0615fd80194205e5c32db4a55bfbce9
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-01 15:40:46 +0100

    Add socket for sending and receiving likes

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -26,22 +26,55 @@ const getUser = (userId: string) => {
 io.on('connection', (socket) => {
   socket.on('add_user', ({ userId }) => {
     addUser(userId, socket.id);
+    console.log(users);
+  });
+
+  socket.on('getOnlineUsers', () => {
+    io.emit('online-users', users);
+  });
+
+  socket.on('send_like', ({ senderId, receiverId, action, message }) => {
+    const user = getUser(receiverId);
+    console.log({ senderId, receiverId, action, message });
+    if (user) {
+      io.in(user.socketId).emit('receive_like', {
+        senderId,
+        receiverId,
+        action,
+        message,
+      });
+    }
   });
 
   socket.on('send_message', ({ senderId, receiverId, message, time }) => {
     const user = getUser(receiverId);
+    if (user) {
+      io.in(user.socketId).emit('notification_message', {
+        senderId,
+        message,
+        receiverId,
+        time,
+        unread: true,
+      });
+    }
+  });
 
+  socket.on('send_message', ({ senderId, receiverId, message, time }) => {
+    const user = getUser(receiverId);
     if (user) {
       io.in(user.socketId).emit('receive_message', {
         senderId,
         message,
+        receiverId,
         time,
+        unread: true,
       });
     }
   });
 
   socket.on('remove_user', ({ userId }) => {
     users = users.filter((user) => user.userId !== userId);
+    console.log(users);
   });
 });
 
