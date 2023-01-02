810580a1e921a564ddcf56902308d4d004c170a4
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-02 16:27:19 +0100

    add updateUnreadNotifications controller for updating read field based on receiverId

--- src/controllers/notificationsController.ts
+++ src/controllers/notificationsController.ts
@@ -1,14 +1,31 @@
 import Notifications from '../models/notificationsModel';
 
 export const getNotificationsByReceiverId = async (req: any, res: any) => {
-  const { receiverId } = req.body;
+  const { receiverId } = req.query;
   try {
     const notifications = await Notifications.find({
       receiver: receiverId,
     }).populate({ path: 'sender', select: ['_id', 'username', 'imageUrl'] });
-
+    console.log(notifications);
     res.status(200).json(notifications);
   } catch (error) {
     res.staus(404).json({ msg: 'Cannot find any any notifications' });
+  }
+};
+
+export const updateUnreadNotifications = async (req: any, res: any) => {
+  const { receiverId } = req.query;
+  try {
+    const notifications = await Notifications.updateMany(
+      {
+        receiver: receiverId,
+      },
+      { read: true },
+      { returnOriginal: false }
+    );
+    console.log(notifications);
+    res.status(200).json({ msg: 'Successfully updated' });
+  } catch (error) {
+    res.status(404).json({ msg: 'Cannot find any any notifications' });
   }
 };
