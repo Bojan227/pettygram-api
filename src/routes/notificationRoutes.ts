5629f5fe6804ff369eeea3dcc281e8a993e43c07
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-02 16:26:01 +0100

    Add notification route for updating read field based on receiverId

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { requireAuth } from '../middleware/requireAuth';
-import { getNotificationsByReceiverId } from '../controllers/notificationsController';
+import { getNotificationsByReceiverId, updateUnreadNotifications } from '../controllers/notificationsController';
 const router = express.Router();
 
 router.get('/', getNotificationsByReceiverId);
+router.put('/', updateUnreadNotifications);
 
 export default router;
