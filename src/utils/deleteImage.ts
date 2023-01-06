ba65f2a90c04b97748ee0e65066a893140236395
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-06 15:33:41 +0100

    Add util function for deleting image from cloudinary

---
+++ src/utils/deleteImage.ts
@@ -0,0 +1,16 @@
+import cloudinary from 'cloudinary';
+import { options } from './uploadImage';
+
+export const deleteImage = async (imageId: string) => {
+  // Use the uploaded file's name as the asset's public ID and
+  // allow overwriting the asset with new versions
+
+  try {
+    // Delete the image
+    const result = await cloudinary.v2.uploader.destroy(imageId, options);
+    console.log(result);
+    return { imgUrl: result.url, img_id: result.public_id };
+  } catch (error) {
+    throw new Error('unable to find the pictureid');
+  }
+};
