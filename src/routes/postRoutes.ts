468f12832d5552fa1b0998d18f24a971c7a10967
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-02-04 12:27:02 +0100

    update route for fetching post by postId

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -14,7 +14,7 @@ import { requireAuth } from '../middleware/requireAuth';
 router.get('/', getPosts);
 
 router.get('/:userId', getPostsByUserId);
-router.get('/:postId', getPostById);
+router.get('/p/:postId', getPostById);
 router.get('/:postId/comments', getSpecificPostComments);
 
 router.post('/', requireAuth, createPost);
