b4a1a0781267ba41b60422bdef9065b7c67a9144
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-02-25 12:12:34 +0100

    add recipe model

---
+++ src/models/recipeModel.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+import User from './userModel';
+
+const RecipeSchema = new Schema({
+  ingredients: [String],
+  createdBy: { type: Schema.Types.ObjectId, ref: User },
+  imageUrl: [String],
+  imageId: [String],
+  createdAt: String,
+  text: String,
+});
+const myDB = mongoose.connection.useDb('insta');
+
+export default myDB.model('Recipe', RecipeSchema);
