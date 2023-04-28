e9a83a09b8a88c931080e80b71ec78ee71b22651
Bojan Blazevski <80133862+Bojan227@users.noreply.github.com>
2023-04-28 17:16:50 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,14 @@ import chatRoutes from './routes/chatRoutes';
 import notificationRoutes from './routes/notificationRoutes';
 import recipeRoutes from './routes/recipeRoutes';
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pettygram.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
